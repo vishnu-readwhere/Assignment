d10f7e78673534b564bf43caad39298819301f92
vishnu-readwhere <vishnu.khandelwal@mediologysoftware.com>
2022-12-12 17:32:17 +0530

    till refresh

--- lib/location_list.dart
+++ lib/location_list.dart
@@ -1,4 +1,4 @@
-import 'dart:html';
+
 
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/location_details.dart';
@@ -15,7 +15,7 @@ class _LocationListState extends State<LocationList>{
   @override
   void initState(){
     super.initState();
-    loadData();
+    loadData();  
   }
 
 
